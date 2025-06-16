8009e3910ad1f85b531dc7937e353efeaf2eeb8b
keiowo <kei8suke@gmail.com>
2025-06-16 23:21:39 +0900

    買い物メモ画面の追加

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="buy_memo"
+        options={{
+          title: '買い物メモ',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
